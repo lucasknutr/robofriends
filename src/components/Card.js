4cf0cb994b4ae956a3dc438a55b57f043ec9a486
Lucas <lucasclaudinocc@gmail.com>
2023-06-26 15:45:07 -0300

    Added basic functionsfor displaying robofriends

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { robots } from "./robots";
 
-const Card = (props) => {
+const Card = ({ name, email, id }) => {
     return (
         <div className="text-center bg-green-300 inline-block rounded-lg hover:transition-all hover:duration-500 origin-center border-3 p-3 m-2 hover:scale-105 cursor-pointer shadow-2xl">
-            <img src={`https://robohash.org/${props.id}`} alt="a robofriend!" />
+            <img src={`https://robohash.org/${id}?200x200`} alt="a robofriend!" className="pb-5"/>
             <div>
-                <h2 className="text-2xl font-bold">{props.name}</h2>
-                <p>{props.email}</p>
+                <h2 className="text-2xl font-bold pb-5">{name}</h2>
+                <p className="pb-5">{email}</p>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Card from './components/Card';
 import { robots } from './components/robots';
+import CardList from './components/CardList.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
-    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} />
-    <Card id={robots[5].id} name={robots[5].name} email={robots[5].email} />
-    <Card id={robots[6].id} name={robots[6].name} email={robots[6].email} />
+    <CardList robots={robots}/>
   </React.StrictMode>
 );
 
